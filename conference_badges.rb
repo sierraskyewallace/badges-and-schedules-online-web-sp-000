0f18a4a31aba48a3ee72661b95e559600c64989e
Sierra Wallace <sskwallace@gmail.com>
2020-08-04 01:17:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(names)
-  names = []
+  names = "Hello, my name is #{name}."
   names.each do |name|
     names << batch_badge_creator(name)
   end
