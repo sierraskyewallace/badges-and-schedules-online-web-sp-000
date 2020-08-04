9e150cac0351ec190648300b56863793c6b6a776
Sierra Wallace <sskwallace@gmail.com>
2020-08-04 01:13:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+names = ["Edsger", "Ada", "Charles", "Alan, "Grace", "Linus, "Matz"]
+
 def batch_badge_creator(array, names)
   array = []
   names.each do |name|
